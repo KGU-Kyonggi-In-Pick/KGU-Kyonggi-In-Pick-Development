d56b8b27c5c5f602cb1c4a0a023fd757aaaf5150
dhwlddj22 <qmfhzhffl22@naver.com>
2024-05-10 16:40:09 +0900

    aass

--- src/pages/VoteLog.page.js
+++ src/pages/VoteLog.page.js
@@ -1,8 +1,20 @@
 // VoteLog.page.js
 import React from "react";
 import VoteData from "../components/VoteData.component";
+import users from "../Data/Data.js";
+// users 데이터를 import
+
+const USER_ROLES = {
+  USER: "user",
+  ADMIN: "admin"
+};
 
 const getEvenClass = (number) => (number % 2 ? "row-even" : "");
+
+const findUserName = (studentId) => {
+  const user = users.find(user => user.StudentID === studentId);
+  return user ? user.name : "알 수 없는 사용자";
+}
 
 const VoteLog = () => {
   const votes = VoteData(); // 투표 데이터 가져오기
@@ -13,7 +25,7 @@ const VoteLog = () => {
       <div className="vote-log-container">
         {votes.map((vote, index) => (
           <div key={index} className={"vote-log-item " + getEvenClass(index)}>
-            <p>{vote.StudentID}님이
+            <p>{findUserName(vote.StudentID)}님이
             {vote.Voted ? " 투표했습니다." : " 투표하지 않았습니다."}</p>
           </div>
         ))}
