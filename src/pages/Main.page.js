ef31113ec128f618cd511f9d6f5cf83b748aaa21
Jang Hee Won <jhw001013@kyonggi.ac.kr>
2024-05-17 18:29:07 +0900

    Update Main.page.js

--- src/pages/Main.page.js
+++ src/pages/Main.page.js
@@ -1,14 +1,12 @@
-// Main.page.js
 import React, { useState } from "react";
-import {
-  MainPageContainer,
-  TemporaryVoteList,
-  TemporaryVoteItem,
-  Button,
-} from "../styles/styled/MainStyles";
+import { MainPageContainer, TemporaryVoteList, TemporaryVoteItem, Button, } from "../styles/styled/MainStyles";
 
+// Main 컴포넌트 정의
 const Main = ({ setCurrentPage }) => {
+  // selectedTemporaryVote 상태와 업데이트 함수 선언
   const [selectedTemporaryVote, setSelectedTemporaryVote] = useState(null);
+
+  // temporaryVotes 배열에 투표 항목들 정의
   const temporaryVotes = [
     "드래곤볼인기투표",
     "컴퓨터공학과 학생회",
@@ -18,28 +16,36 @@ const Main = ({ setCurrentPage }) => {
     "연극영화과 학생회",
   ];
 
+  // 투표 항목 클릭 시 selectedTemporaryVote 상태 업데이트
   const handleTemporaryVoteClick = (vote) => {
     setSelectedTemporaryVote(vote);
   };
 
+  // 투표하기 버튼 클릭 시 실행되는 함수
   const handleVoteButtonClick = () => {
     if (selectedTemporaryVote) {
+      // 투표 항목이 선택된 경우 vote 페이지로 이동
       setCurrentPage("vote");
     } else {
+      // 투표 항목이 선택되지 않은 경우 경고 메시지 출력
       alert("투표할 항목을 선택하세요.");
     }
   };
 
+  // 결과보기 버튼 클릭 시 실행되는 함수
   const handleResultsButtonClick = () => {
     if (selectedTemporaryVote) {
+      // 투표 항목이 선택된 경우 results 페이지로 이동
       setCurrentPage("results");
     } else {
+      // 투표 항목이 선택되지 않은 경우 경고 메시지 출력
       alert("투표할 항목을 선택하세요.");
     }
   };
 
   return (
     <MainPageContainer>
+      {/* 투표 항목 목록 렌더링 */}
       <TemporaryVoteList>
         {temporaryVotes.map((vote, index) => (
           <TemporaryVoteItem
@@ -51,9 +57,11 @@ const Main = ({ setCurrentPage }) => {
           </TemporaryVoteItem>
         ))}
       </TemporaryVoteList>
+
+      {/* 투표하기 버튼 렌더링 */}
       <Button
         isDisabled={selectedTemporaryVote === null}
-        onClick={handleVoteButtonClick}
+        onClick={handleVoteButtonClick} 
         aria-disabled={selectedTemporaryVote === null}
         aria-label={
           selectedTemporaryVote ? "Click to vote" : "Select a vote option first"
@@ -61,6 +69,8 @@ const Main = ({ setCurrentPage }) => {
       >
         투표하기
       </Button>
+
+      {/* 결과보기 버튼 렌더링 */}
       <Button
         isDisabled={selectedTemporaryVote === null}
         onClick={handleResultsButtonClick}
